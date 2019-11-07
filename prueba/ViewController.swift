e6a06041c0f2b5be7c3d10054f18c30da216e237
irving <cdam02@MacBook-Pro-de-CDAM02.local>
2019-11-07 16:44:01 -0600

    Primer commit

---
+++ prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by CEDAM02 on 07/11/19.
+//  Copyright © 2019 UNAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
